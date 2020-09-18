414717845d48f113b75dc0f974c8123ddaebb85f
Inbar Lei <inbar.mada@gmail.com>
2020-09-18 14:31:59 -0500

    Create calculator design in jsx and css

--- src/App.js
+++ src/App.js
@@ -6,18 +6,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="calc-body">
+          <div className="cell result-display">
+            0.23
+          </div>
+          <div className="grid-holder">
+            <div className="cell AC" style={{gridArea: "AC"}}>AC</div>
+            <div className="cell operator" style={{gridArea: "divide"}}>/</div>
+            <div className="cell operator" style={{gridArea: "mult"}}>x</div>
+            <div className="cell number" style={{gridArea: "seven"}}>7</div>
+            <div className="cell number" style={{gridArea: "eight"}}>8</div>
+            <div className="cell number" style={{gridArea: "nine"}}>9</div>
+            <div className="cell operator" style={{gridArea: "minus"}}>-</div>
+            <div className="cell number" style={{gridArea: "four"}}>4</div>
+            <div className="cell number" style={{gridArea: "five"}}>5</div>
+            <div className="cell number" style={{gridArea: "six"}}>6</div>
+            <div className="cell operator" style={{gridArea: "plus"}}>+</div>
+            <div className="cell  number" style={{gridArea: "one"}}>1</div>
+            <div className="cell number" style={{gridArea: "two"}}>2</div>
+            <div className="cell number" style={{gridArea: "three"}}>3</div>
+            <div className="cell number" style={{gridArea: "zero"}}>0</div>
+            <div className="cell number" style={{gridArea: "dec"}}>.</div>
+            <div className="cell equal" style={{gridArea: "equal"}}>=</div>
+          </div>
+        </div>
       </header>
     </div>
   );
